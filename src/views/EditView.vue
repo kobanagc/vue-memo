4417aa0b55c9e8bf7e9cf5cbd35eeee5ce3f117b
kobanagc <naoki.kobayashi.0708@gmail.com>
2022-08-29 23:46:28 +0900

    edit view

---
+++ src/views/EditView.vue
@@ -0,0 +1,15 @@
+<template>
+Edit View {{ $route.params.id }}
+</template>
+
+<script>
+export default {
+  name: 'EditView',
+  computed: {
+    memo() {
+      const id = parseInt(this.$route.params.id)
+      return this.$store.getters.getMemoById(id)
+    }
+  }
+}
+</script>
