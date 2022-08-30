98473bfccd2bdae5cb44872d46e83d0a1f506c7d
kobanagc <naoki.kobayashi.0708@gmail.com>
2022-08-30 22:58:26 +0900

    edit

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,10 +1,15 @@
 <template>
-Edit View {{ $route.params.id }}
+  <MemoForm v-if="memo" :memo="memo" />
+  <p v-else>指定されたメモはありません。</p>
 </template>
 
 <script>
+import MemoForm from '@/components/MemoForm.vue'
 export default {
   name: 'EditView',
+  components: {
+    MemoForm
+  },
   computed: {
     memo() {
       const id = parseInt(this.$route.params.id)
